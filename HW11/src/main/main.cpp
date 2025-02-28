d008519efdc7f05a6d478d53ec6aa66498994c2e
DiCode77 <DiCode77>
2025-02-28 15:35:53 +0200

    Add main

---
+++ HW11/src/main/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "../lib/lib.hpp"
+
+
+int main(){
+    std::cout << "Generate a password of <n> characters:\t" << getRandPassword(20) << std::endl;
+    std::cout << "Generate a password of <n> characters:\t" << getRandPassword(20) << std::endl;
+    std::cout << "Generate a password of <n> characters:\t" << getRandPassword(20) << std::endl;
+    std::cout << "Generate a password of <n> characters:\t" << getRandPassword(20) << std::endl;
+    
+    
+    return 0;
+}
